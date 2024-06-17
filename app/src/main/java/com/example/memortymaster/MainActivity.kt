7a7f01eb3cc9dad664c3940b31481ee774111b57
Yusuf Senturk <yusufsengs8@gmail.com>
2024-06-17 21:26:19 +0300

    First Commit

---
+++ app/src/main/java/com/example/memortymaster/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.memortymaster
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.memortymaster.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+    }
+}
